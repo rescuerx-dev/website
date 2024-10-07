17f5a727915b2708a6a6aaa9d948c8f1e2b277cd
Shreyasi <shreyasi.patil@rescuerx.in>
2024-10-07 12:57:47 +0530

    test: contact page form

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -9,8 +9,8 @@ export async function handler(event) {
     const transporter = createTransport({
       service: 'Gmail',
       auth: {
-        user: 'contact@rescuerx.in', // Your Gmail address
-        pass: 'bpbutsiuoynuixkr', // Your Gmail password or an app password
+        user: process.env.EMAIL_USER, // Your Gmail address
+        pass: process.env.EMAIL_PASS, // Your Gmail password or an app password
       },
     });
 
