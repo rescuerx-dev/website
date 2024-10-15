3e8e0febaa3476d184b466ce819179af1fd9b60f
Shreyasi <shreyasi.patil@rescuerx.in>
2024-10-16 02:41:28 +0530

    css: contact page form

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,8 @@ const submitBtn = document.getElementById('submit');
 const errorMsg = document.querySelector('.error-msg');
 document.getElementById('contact-form').addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevent default form submission
-    
+    submitBtn.textContent = 'Submitting...'
+    submitBtn.classList.toggle('submitting');
     const form = event.target;
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
@@ -18,12 +19,14 @@ document.getElementById('contact-form').addEventListener('submit', async (event)
 
     
     if (response.ok) {
-      submitBtn.textContent = 'Submitted!';
+      submitBtn.textContent = 'Submitted ✔';
+      submitBtn.classList.toggle('submitted');
 
       form.reset(); // Optionally reset the form
     } 
     
     } catch (error) {
+      submitBtn.textContent = 'Submit now';
       errorMsg.classList.toggle('show');
     }
     });
